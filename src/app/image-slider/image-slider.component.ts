5d930de6c2193382a2eee1af2274353f35a107e0
Irtiza Chowdhury <153156568+ich-wtag@users.noreply.github.com>
2024-04-29 10:25:43 +0600

    refactor : code updated for go to next and go to prev function

--- src/app/image-slider/image-slider.component.ts
+++ src/app/image-slider/image-slider.component.ts
@@ -5,27 +5,27 @@ import {
   transition,
   trigger,
 } from '@angular/animations';
-import { AfterContentInit, Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 const animationCarousel = [
-  state('outright', style({ transform: `translateX(100%)` })),
-  state('outleft', style({ transform: `translateX(-100%)` })),
-  transition('void=>inleft', [style({ transform: `translateX(0)` })]),
-  transition('void=>outleft', [style({ transform: `translateX(-100%)` })]),
+  state('outright', style({ transform: 'translateX(100%)' })),
+  state('outleft', style({ transform: 'translateX(-100%)' })),
+  transition('void=>inleft', [style({ transform: 'translateX(0)' })]),
+  transition('void=>outleft', [style({ transform: 'translateX(-100%)' })]),
 
   transition('*=>inright', [
-    style({ transform: `translateX(-100%)` }),
-    animate('260ms ease-in', style({ transform: `translateX(0)` })),
+    style({ transform: 'translateX(-100%)' }),
+    animate('300ms ease-in', style({ transform: 'translateX(0)' })),
   ]),
   transition('*=>inleft', [
-    style({ transform: `translateX(100%)` }),
-    animate('260ms ease-in', style({ transform: `translateX(0)` })),
+    style({ transform: 'translateX(100%)' }),
+    animate('300ms ease-in', style({ transform: 'translateX(0)' })),
   ]),
   transition('*=>outleft', [
-    animate('260ms ease-in', style({ transform: `translateX(-100%)` })),
+    animate('300ms ease-in', style({ transform: 'translateX(-100%)' })),
   ]),
   transition('*=>outright', [
-    animate('260ms ease-in', style({ transform: `translateX(100%)` })),
+    animate('300ms ease-in', style({ transform: 'translateX(100%)' })),
   ]),
 ];
 
@@ -33,72 +33,39 @@ const animationCarousel = [
   selector: 'app-image-slider',
   templateUrl: './image-slider.component.html',
   styleUrls: ['./image-slider.component.scss'],
-  animations: [
-    trigger('slideInOut', animationCarousel),
-    // trigger('slideInOut', [
-    //   transition('void => *', [
-    //     style({ opacity: 0 }),
-    //     animate('500ms', style({ opacity: 1 })),
-    //   ]),
-    //   transition('* => void', [animate('500ms', style({ opacity: 0 }))]),
-    // ]),
-  ],
+  animations: [trigger('slideInOut', animationCarousel)],
 })
-export class ImageSliderComponent implements OnInit, AfterContentInit {
+export class ImageSliderComponent implements OnInit {
   currentImageIndex: number = 0;
-  slideControl!: any[];
+  slideControl!: string[];
 
   @Input() imageSource!: any[];
 
-  // getDesiredImage() {
-  //   return this.imageSource[this.currentImageIndex];
-  // }
+  goToNextSlide() {
+    const lastIndex = this.imageSource.length - 1;
+    const prevIndex = this.currentImageIndex;
+    const newIndex = ++this.currentImageIndex;
+    this.currentImageIndex = newIndex > lastIndex ? 0 : newIndex;
 
-  // goToNextSlide() {
-  //   const lastIndex = this.imageSource.length - 1;
-  //   const newIndex = ++this.currentImageIndex;
-  //   this.currentImageIndex = newIndex > lastIndex ? 0 : newIndex;
+    this.onSlide(this.currentImageIndex, prevIndex, 'right');
+  }
 
-  //   this.getDesiredImage();
-  // }
+  goToPreviousSlide() {
+    const lastIndex = this.imageSource.length - 1;
+    const prevIndex = this.currentImageIndex;
+    const newIndex = --this.currentImageIndex;
+    this.currentImageIndex = newIndex < 0 ? lastIndex : newIndex;
 
-  // goToPreviousSlide() {
-  //   const lastIndex = this.imageSource.length - 1;
-  //   const newIndex = --this.currentImageIndex;
-  //   this.currentImageIndex = newIndex < 0 ? lastIndex : newIndex;
-
-  //   this.getDesiredImage();
-  // }
+    this.onSlide(this.currentImageIndex, prevIndex, 'left');
+  }
   ngOnInit(): void {
     this.slideControl = this.imageSource.map((x: any, index: number) =>
       index ? 'outleft' : 'inleft'
     );
-    console.log(this.slideControl);
   }
 
-  ngAfterContentInit(): void {
-    // this.slideControl = this.imageSource.map((x: any, index: number) =>
-    //   index ? 'outleft' : 'inleft'
-    // );
-    // console.log('after', this.slideControl, this.imageSource);
-  }
-
-  change(direction: string) {
-    const incr = direction == 'right' ? -1 : +1;
-    const prev = this.currentImageIndex;
-    this.currentImageIndex =
-      (this.currentImageIndex + this.imageSource.length + incr) %
-      this.imageSource.length;
-
-    this.onSlide(this.currentImageIndex, prev, direction);
-  }
-
-  onSlide(current: any, prev: any, direction: any) {
+  onSlide(current: number, prev: number, direction: string) {
     this.slideControl[current] = 'in' + direction;
     this.slideControl[prev] = 'out' + direction;
-
-    console.log(this.slideControl);
-
-    // this.posThumbail(current);
   }
 }
